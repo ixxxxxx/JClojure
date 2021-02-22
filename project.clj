e4debc8777a4d8bf0325a7d8bf501bde6c41b498
ixxxxxx <u.lostland@gmail.com>
2021-02-22 14:16:46 +0800

    use java envirment

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject sample "0.1.0-SNAPSHOT"
   :description "A sample application for leingen"
-  :dependencies [[org.clojure/clojure "1.9.0"]]
-  :source-paths ["src/main/clojure"]
-  :main jay.mars.app)
+  :java-source-paths ["src/main/java"]
+  :main jay.mars.App)
---
+++ src/main/java/jay/mars/App.java
@@ -0,0 +1,7 @@
+package jay.mars;
+
+public class App{
+    public static void main(String[] args){
+        System.out.println("Hello, Leiningen from java.");
+    }
+}

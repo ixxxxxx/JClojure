23e6fb030169958c500b72e935f4667f5f2c4e59
ixxxxxx <u.lostland@gmail.com>
2021-03-24 12:43:55 +0800

    add test

---
+++ messages/src/main/java/jay/mars/market/messages/DropSequence.java
@@ -0,0 +1,13 @@
+package jay.mars.market.messages;
+
+public class DropSequence {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
